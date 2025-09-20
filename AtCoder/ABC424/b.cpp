6ab3c126bebb5e3a82cb5731529aec605444972d
szknuts <xeno4absolute@gmail.com>
2025-09-20 23:09:15 +0900

    abc

---
+++ AtCoder/ABC424/b.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    int A, B;
+    vector<int> S(N);
+    vector<int> ANS;
+
+    for (int i = 0; i < K; i++)
+    {
+        cin >> A >> B;
+        S[A - 1]++;
+        if (S[A - 1] == M)
+            ANS.push_back(A);
+    }
+
+    for (int i = 0; i < ANS.size(); i++)
+        cout << ANS[i] << " ";
+    if (ANS.size() > 0)
+        cout << endl;
+}
