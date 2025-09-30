63ca4646281e60dce15b48372d10a7cf7bf3e2dc
szknuts <xeno4absolute@gmail.com>
2025-09-30 21:58:19 +0900

    notion-clone: -

---
+++ notion-clone/src/modules/auth/auth.repository.ts
@@ -0,0 +1,27 @@
+import { supabase } from "@/lib/supabase";
+
+export const authRepository = {
+  async sigunup(name: string, email: string, password: string) {
+    const { data, error } = await supabase.auth.signUp({
+      email,
+      password,
+      options: { data: { name } },
+    });
+    if (error != null || data.user == null) throw new Error(error?.message);
+    return {
+      ...data.user, //スプレッド構文
+      userName: data.user.user_metadata.name,
+    };
+  },
+  async signin(email: string, password: string) {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    if (error != null || data.user == null) throw new Error(error?.message);
+    return {
+      ...data.user,
+      userName: data.user.user_metadata.name,
+    };
+  },
+};
